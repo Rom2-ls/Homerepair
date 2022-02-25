57fafe956e191712a3cfdf6ec8fecb4962f181bc
benimambiguerin <beni.mambiguerin@gmail.com>
2022-02-25 15:50:26 +0100

    Ajout de l'accueil et de la page de connexion et du formulaire

---
+++ homerepair/lib/social_page.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:homerepair/main.dart';
+import 'package:homerepair/delayed_animation.dart';
+import 'package:homerepair/welcome_page.dart';
+import 'package:homerepair/login_page.dart';
+import 'package:homerepair/social_page.dart';
+
+class SocialPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white.withOpacity(0),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            DelayedAnimation(
+              delay: 1000,
+              child: Container(
+                height: 300,
+                child: Image.asset('images/repairbike.png'),
+              ),
+            ),
+            DelayedAnimation(
+              delay: 2000,
+              child: Container(
+                height: 200,
+                child: Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 50,
+                    horizontal: 30,
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        "On s'occupe de tout",
+                        style: TextStyle(
+                          color: d_red,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Un réparateur ou une réparatrice vous retrouve à l'adresse indiquée !",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            DelayedAnimation(
+              delay: 2500,
+              child: Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 14,
+                  horizontal: 40,
+                ),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          primary: d_red,
+                          padding: EdgeInsets.all(13)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.mail_outline_outlined),
+                          SizedBox(width: 10),
+                          Text(
+                            'CONNEXION PAR EMAIL',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          primary: Colors.white,
+                          padding: EdgeInsets.all(13)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('images/googlelogo.png', height: 20),
+                          SizedBox(width: 10),
+                          Text(
+                            'CONNEXION AVEC GOOGLE',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          primary: Color.fromARGB(255, 7, 135, 255),
+                          padding: EdgeInsets.all(13)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FaIcon(FontAwesomeIcons.facebook),
+                          SizedBox(width: 10),
+                          Text(
+                            'CONNEXION AVEC FACEBOOK',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ homerepair/lib/welcome_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:homerepair/main.dart';
+import 'package:homerepair/delayed_animation.dart';
+import 'package:homerepair/welcome_page.dart';
+import 'package:homerepair/login_page.dart';
+import 'package:homerepair/social_page.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEDECF2),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: 30,
+            vertical: 60,
+          ),
+          child: Column(
+            children: [
+              DelayedAnimation(
+                delay: 1500,
+                child: Container(
+                  height: 170,
+                  child: Image.asset('images/homerepairlogo.png'),
+                ),
+              ),
+              DelayedAnimation(
+                delay: 2500,
+                child: Container(
+                  height: 400,
+                  child: Image.asset('images/imageaccueil.png'),
+                ),
+              ),
+              DelayedAnimation(
+                delay: 3500,
+                child: Container(
+                  margin: EdgeInsets.only(
+                    top: 30,
+                    bottom: 20,
+                  ),
+                  child: Text(
+                    "On répare vos vélos et trotinettes, chez vous, partout",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              DelayedAnimation(
+                delay: 4500,
+                child: Container(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: d_red,
+                        shape: StadiumBorder(),
+                        padding: EdgeInsets.all(13)),
+                    child: Text("C'EST PARTI"),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SocialPage(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
