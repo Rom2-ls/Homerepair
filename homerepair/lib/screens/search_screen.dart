6ac797d0f79ba7823f541b94d0cb5db4ee1ab501
Rom2-ls <capolunghiromain@gmail.com>
2022-03-19 18:54:20 +0100

    fix conflict

--- homerepair/lib/screens/search_screen.dart
+++ homerepair/lib/screens/search_screen.dart
@@ -17,11 +17,7 @@ class _SearchScreenState extends State<SearchScreen> {
           centerTitle: true,
         ),
         body: const Center(
-<<<<<<< HEAD
           child: GetService(define_collection: "Services"),
-=======
-          child: GetService(define_status: ""),
->>>>>>> e3293c3c03a74d1b7059225398732333b1b74b18
         ));
   }
 }
