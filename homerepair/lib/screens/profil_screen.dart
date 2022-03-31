dbf9c46c68274917af993ab28c4014eabdebb93f
Rom2-ls <capolunghiromain@gmail.com>
2022-03-31 16:26:34 +0200

    Affichage et modification des données sur la page de profil

--- homerepair/lib/screens/profil_screen.dart
+++ homerepair/lib/screens/profil_screen.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:homerepair/model/user_model.dart';
 import 'package:homerepair/screens/become_repair_screen.dart';
 import 'package:homerepair/screens/create_service_screen.dart';
@@ -19,9 +19,25 @@ class _ProfilScreenState extends State<ProfilScreen> {
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedUser = UserModel();
 
+  final firstnameController = TextEditingController();
+  final lastnameController = TextEditingController();
+  final numberController = TextEditingController();
+
+  late FocusNode firstnameFocus;
+  late FocusNode lastnameFocus;
+  late FocusNode numberFocus;
+
+  late bool enableFN;
+  late bool enableLN;
+  late bool enableNB;
+
+  late Color colorButton;
+  late TextStyle styleButton;
+
+  late String email = "";
+
   @override
   void initState() {
-    super.initState();
     FirebaseFirestore.instance
         .collection("users")
         .doc(user!.uid)
@@ -30,6 +46,18 @@ class _ProfilScreenState extends State<ProfilScreen> {
       loggedUser = UserModel.fromMap(value.data());
       setState(() {});
     });
+    firstnameFocus = FocusNode();
+    lastnameFocus = FocusNode();
+    numberFocus = FocusNode();
+
+    enableFN = false;
+    enableLN = false;
+    enableNB = false;
+
+    colorButton = Colors.white;
+    styleButton = const TextStyle(color: Colors.grey);
+
+    super.initState();
   }
 
   @override
@@ -41,6 +69,161 @@ class _ProfilScreenState extends State<ProfilScreen> {
         return const ClientOption();
       }
     }
+
+    if (loggedUser.email != null) {
+      email = loggedUser.email!;
+    }
+    String repair = "Vous n'êtes pas réparateur";
+    if (loggedUser.repair == true) {
+      repair = "Vous êtes réparateur";
+    }
+
+    final firstnameField = Row(
+      children: [
+        SizedBox(
+          width: 200,
+          height: 40,
+          child: TextFormField(
+            decoration: InputDecoration(
+              icon: const Icon(Icons.person),
+              border: InputBorder.none,
+              hintText: loggedUser.firstname,
+              labelStyle: TextStyle(
+                color: Colors.grey[400],
+              ),
+            ),
+            enabled: enableFN,
+            focusNode: firstnameFocus,
+            controller: firstnameController,
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+            onSaved: (value) {
+              firstnameController.text = value!;
+            },
+            onFieldSubmitted: (value) {
+              setState(() {
+                enableFN = !enableFN;
+              });
+            },
+          ),
+        ),
+        GestureDetector(
+            child: const Icon(Icons.edit),
+            onTap: () {
+              setState(() {
+                enableFN = !enableFN;
+              });
+            })
+      ],
+    );
+
+    final lastnameField = Row(
+      children: [
+        SizedBox(
+          width: 200,
+          height: 40,
+          child: TextFormField(
+            decoration: InputDecoration(
+              icon: const Icon(Icons.person),
+              border: InputBorder.none,
+              hintText: loggedUser.lastname,
+              labelStyle: TextStyle(
+                color: Colors.grey[400],
+              ),
+            ),
+            enabled: enableLN,
+            focusNode: lastnameFocus,
+            controller: lastnameController,
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+            onSaved: (value) {
+              lastnameController.text = value!;
+            },
+            onFieldSubmitted: (value) {
+              setState(() {
+                enableLN = !enableLN;
+              });
+            },
+          ),
+        ),
+        GestureDetector(
+            child: const Icon(Icons.edit),
+            onTap: () {
+              lastnameFocus.requestFocus();
+              setState(() {
+                enableLN = !enableLN;
+              });
+            })
+      ],
+    );
+
+    final numberField = Row(
+      children: [
+        SizedBox(
+          width: 200,
+          height: 40,
+          child: TextFormField(
+            decoration: InputDecoration(
+              icon: const Icon(Icons.phone_rounded),
+              border: InputBorder.none,
+              hintText: "number",
+              labelStyle: TextStyle(
+                color: Colors.grey[400],
+              ),
+            ),
+            enabled: enableNB,
+            focusNode: numberFocus,
+            controller: numberController,
+            keyboardType: TextInputType.number,
+            textInputAction: TextInputAction.done,
+            onSaved: (value) {
+              numberController.text = value!;
+            },
+            onFieldSubmitted: (value) {
+              setState(() {
+                enableNB = !enableNB;
+              });
+            },
+          ),
+        ),
+        GestureDetector(
+            child: const Icon(Icons.edit),
+            onTap: () {
+              numberFocus.requestFocus();
+              setState(() {
+                enableNB = !enableNB;
+              });
+            })
+      ],
+    );
+
+    final emailField = Row(
+      children: [const Icon(Icons.email), Text(email)],
+    );
+
+    final repairField = Row(
+      children: [const Icon(Icons.work), Text(repair)],
+    );
+
+    final updateProfil = ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          primary: colorButton,
+        ),
+        onPressed: () {
+          if (firstnameController.text.isNotEmpty ||
+              lastnameController.text.isNotEmpty ||
+              numberController.text.isNotEmpty) {
+            setState(() {
+              colorButton = Colors.blueAccent;
+              styleButton = const TextStyle(color: Colors.white);
+            });
+          }
+        },
+        child: Text(
+          "Modifier",
+          style: styleButton,
+        ));
 
     final logoutButton = OutlinedButton(
       style: ElevatedButton.styleFrom(primary: Colors.red),
@@ -85,11 +268,18 @@ class _ProfilScreenState extends State<ProfilScreen> {
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text("${loggedUser.firstname}"),
-              Text("${loggedUser.repair}"),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                firstnameField,
+                lastnameField,
+                numberField,
+                emailField,
+                repairField,
+                updateProfil,
+              ],
+            ),
           ),
         ),
       ),
